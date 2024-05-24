f7d5247cd1477281c71daeee7097f0a1e9990c89
sanathsharma <48090208+sanathsharma@users.noreply.github.com>
2024-05-25 04:07:31 +0530

    feat(nvim): highlight 120th column for text overflow alert

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -12,6 +12,10 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.conceallevel = 2
 
+-- color 120th column to identify formatter overflow
+vim.opt.colorcolumn = "120"
+vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#24273a" })
+
 vim.opt.foldmethod = "manual"
 -- vim.o.foldcolumn = "1"
 vim.o.foldlevel = 99 -- Using ufo provider need a large value
