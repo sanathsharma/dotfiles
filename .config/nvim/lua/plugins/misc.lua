7d008572792fcff7526d18aef1949e2113bff9bb
sanathsharma <48090208+sanathsharma@users.noreply.github.com>
2024-07-15 10:29:54 +0530

    feat(nvim): add RRethy/vim-illuminate plugin for higlighting and jumping between variable references

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,6 +1,6 @@
 return {
 	{ "tpope/vim-unimpaired", event = "VeryLazy" },
-	{ "tpope/vim-surround", event = "VeryLazy" },
+	{ "tpope/vim-surround",   event = "VeryLazy" },
 	{
 		"folke/trouble.nvim",
 		event = "VeryLazy",
@@ -39,13 +39,13 @@ return {
 		"nvim-treesitter/nvim-treesitter-context",
 		config = function()
 			require("treesitter-context").setup({
-				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-				max_lines = 10, -- How many lines the window should span. Values <= 0 mean no limit.
+				enable = true,       -- Enable this plugin (Can be enabled/disabled later via commands)
+				max_lines = 10,      -- How many lines the window should span. Values <= 0 mean no limit.
 				min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
 				line_numbers = true,
 				multiline_threshold = 5, -- Maximum number of lines to show for a single context
 				trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-				mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+				mode = "cursor",     -- Line used to calculate context. Choices: 'cursor', 'topline'
 				-- Separator between context and content. Should be a single character string, like '-'.
 				-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
 				separator = nil,
@@ -62,4 +62,5 @@ return {
 			vim.api.nvim_set_hl(0, "TreesitterContextLineNumberBottom", { underline = false })
 		end,
 	},
+	"RRethy/vim-illuminate",
 }
