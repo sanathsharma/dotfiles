bc324a600398e743884599d786c3874081ed564f
sanathsharma <48090208+sanathsharma@users.noreply.github.com>
2024-07-18 14:03:14 +0530

    feat(nvim): add nvim-autopairs for auto inserting closing braces and quotes etc.

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -92,4 +92,11 @@ return {
 			})
 		end,
 	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		-- use opts = {} for passing setup options
+		-- this is equalent to setup({}) function
+	},
 }
