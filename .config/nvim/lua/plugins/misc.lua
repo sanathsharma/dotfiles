223d212f97e47354449954971d8cc5f7998b15fc
sanathsharma <48090208+sanathsharma@users.noreply.github.com>
2024-08-11 23:51:41 +0530

    feat(nvim): add noice plugin for floating command line ++
    - add emmet and tailwind lsp

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,6 +1,6 @@
 return {
 	{ "tpope/vim-unimpaired", event = "VeryLazy" },
-	{ "tpope/vim-surround",   event = "VeryLazy" },
+	{ "tpope/vim-surround", event = "VeryLazy" },
 	{
 		"folke/trouble.nvim",
 		event = "VeryLazy",
@@ -39,13 +39,13 @@ return {
 		"nvim-treesitter/nvim-treesitter-context",
 		config = function()
 			require("treesitter-context").setup({
-				enable = true,       -- Enable this plugin (Can be enabled/disabled later via commands)
-				max_lines = 10,      -- How many lines the window should span. Values <= 0 mean no limit.
+				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+				max_lines = 10, -- How many lines the window should span. Values <= 0 mean no limit.
 				min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
 				line_numbers = true,
 				multiline_threshold = 5, -- Maximum number of lines to show for a single context
 				trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-				mode = "cursor",     -- Line used to calculate context. Choices: 'cursor', 'topline'
+				mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
 				-- Separator between context and content. Should be a single character string, like '-'.
 				-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
 				separator = nil,
@@ -114,5 +114,36 @@ return {
 				},
 			})
 		end,
+	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				},
+			},
+			-- you can enable a preset for easier configuration
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
+			},
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			-- "rcarriga/nvim-notify",
+		},
 	},
 }
