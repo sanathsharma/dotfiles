19785353fe0e65cc837598fb250063e24496524b
sanathsharma <48090208+sanathsharma@users.noreply.github.com>
2024-09-24 21:35:12 +0530

    feat(nvim): lazygit and lazydocker short keymap to open in zellij floating pane

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -2,12 +2,12 @@ return {
 	{
 		"numToStr/Comment.nvim",
 		keys = {
-			{ "gcc", mode = "n",          desc = "Comment toggle current line" },
-			{ "gc",  mode = { "n", "o" }, desc = "Comment toggle linewise" },
-			{ "gc",  mode = "x",          desc = "Comment toggle linewise (visual)" },
-			{ "gbc", mode = "n",          desc = "Comment toggle current block" },
-			{ "gb",  mode = { "n", "o" }, desc = "Comment toggle blockwise" },
-			{ "gb",  mode = "x",          desc = "Comment toggle blockwise (visual)" },
+			{ "gcc", mode = "n", desc = "Comment toggle current line" },
+			{ "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
+			{ "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
+			{ "gbc", mode = "n", desc = "Comment toggle current block" },
+			{ "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
+			{ "gb", mode = "x", desc = "Comment toggle blockwise (visual)" },
 			{
 				"<leader>/",
 				function()
@@ -22,6 +22,9 @@ return {
 				desc = "Toggle comment",
 			},
 		},
+		config = function(_, opts)
+			require("Comment").setup(opts)
+		end,
 	},
 	{
 		"folke/todo-comments.nvim",
