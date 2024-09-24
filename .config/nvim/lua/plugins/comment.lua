c3c492c29a0705285b9d95d3a69eb127ef7b7a51
sanathsharma <48090208+sanathsharma@users.noreply.github.com>
2024-09-24 13:47:15 +0530

    feat(kitty): add kitty config

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -2,25 +2,25 @@ return {
 	{
 		"numToStr/Comment.nvim",
 		keys = {
-			{ "gcc", mode = "n", desc = "Comment toggle current line" },
-			{ "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
-			{ "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
-			{ "gbc", mode = "n", desc = "Comment toggle current block" },
-			{ "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
-			{ "gb", mode = "x", desc = "Comment toggle blockwise (visual)" },
+			{ "gcc", mode = "n",          desc = "Comment toggle current line" },
+			{ "gc",  mode = { "n", "o" }, desc = "Comment toggle linewise" },
+			{ "gc",  mode = "x",          desc = "Comment toggle linewise (visual)" },
+			{ "gbc", mode = "n",          desc = "Comment toggle current block" },
+			{ "gb",  mode = { "n", "o" }, desc = "Comment toggle blockwise" },
+			{ "gb",  mode = "x",          desc = "Comment toggle blockwise (visual)" },
 			{
-        "<leader>/",
-        function()
-          require("Comment.api").toggle.linewise.current()
-        end,
-        desc = "Toggle comment"
-      },
-      {
-        "<leader>/",
-          "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
-        mode = "v",
-        desc = "Toggle comment",
-      }
+				"<leader>/",
+				function()
+					require("Comment.api").toggle.linewise.current()
+				end,
+				desc = "Toggle comment",
+			},
+			{
+				"<leader>/",
+				"<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+				mode = "v",
+				desc = "Toggle comment",
+			},
 		},
 	},
 	{
