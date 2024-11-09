746b41c6a8cf351f46c38b322db6582f797cf5e0
sanathsharma <48090208+sanathsharma@users.noreply.github.com>
2024-11-09 11:52:51 +0530

    feat(nvim): add react component creation snippets w/ luasnip

--- .config/nvim/lua/user-cmds.lua
+++ .config/nvim/lua/user-cmds.lua
@@ -2,12 +2,12 @@
 vim.api.nvim_create_user_command("CopyCurrentBufferFullPath", function()
 	local path = vim.fn.fnamemodify(vim.fn.expand("%"), ":p")
 	vim.fn.setreg("+", path) -- put path to + register (clipboard)
-	vim.notify('Copied "' .. path .. '" to the clipboard!')
+	vim.notify("Copied \"" .. path .. "\" to the clipboard!")
 end, { nargs = 0 })
 vim.api.nvim_create_user_command("CopyCurrentBufferRelPath", function()
 	local path = vim.fn.fnamemodify(vim.fn.expand("%"), "")
 	vim.fn.setreg("+", path) -- put path to + register (clipboard)
-	vim.notify('Copied "' .. path .. '" to the clipboard!')
+	vim.notify("Copied \"" .. path .. "\" to the clipboard!")
 end, { nargs = 0 })
 
 vim.api.nvim_create_user_command("KillPort", function(parms)
@@ -19,3 +19,7 @@ vim.api.nvim_create_user_command("MakeExecutable", function()
 	vim.cmd("!chmod +x %")
 end, { nargs = 0 })
 
+-- source snippets
+vim.api.nvim_create_user_command("SourceSnippets", function()
+	vim.cmd("luafile ~/.config/nvim/lua/snippets/init.lua")
+end, { nargs = 0 })
