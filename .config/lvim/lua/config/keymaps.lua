6c4f3a855484f48c528efb7ffd52c6d07626686e
sanathsharma <48090208+sanathsharma@users.noreply.github.com>
2025-05-04 16:48:45 +0530

    feat(lvim): update config, helix like
    add codeium for ai tab suggestions

--- .config/lvim/lua/config/keymaps.lua
+++ .config/lvim/lua/config/keymaps.lua
@@ -1,3 +1,50 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- move selected lines up and down in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- exit from insert mode
+vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true })
+
+-- half page jump
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- search to stay in middle when switching back and forth between them
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- keep old copied content even after pasting it over another selected content
+vim.keymap.set("x", "<leader>p", '"_dP', { desc = "Past over selection w/o loosing the clipboard content" })
+
+-- stay in vim mode after command execution
+vim.api.nvim_set_keymap("v", "<", "<gv", { noremap = true })
+vim.api.nvim_set_keymap("v", ">", ">gv", { noremap = true })
+
+vim.api.nvim_set_keymap("n", "ge", "G", { noremap = true })
+vim.api.nvim_set_keymap("v", "ge", "G", { noremap = true })
+vim.api.nvim_set_keymap("n", "d", "x", { noremap = true })
+vim.api.nvim_set_keymap("n", "x", "V$", { noremap = true })
+vim.api.nvim_set_keymap("n", "m", "v", { noremap = true })
+vim.api.nvim_set_keymap("n", "gh", "0", { noremap = true })
+vim.api.nvim_set_keymap("v", "gh", "0", { noremap = true })
+vim.api.nvim_set_keymap("n", "gl", "$", { noremap = true })
+vim.api.nvim_set_keymap("v", "gl", "$", { noremap = true })
+vim.api.nvim_set_keymap("n", "gs", "^", { noremap = true })
+vim.api.nvim_set_keymap("v", "gs", "^", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>c", "gcc", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>C", "gbc", { noremap = true })
+vim.api.nvim_set_keymap("v", "<leader>c", "gc", { noremap = true })
+vim.api.nvim_set_keymap("v", "<leader>C", "gb", { noremap = true })
+-- check if selection spans from first letter of first line to last letter of last line
+-- If yes, apply j$ (move down and to end of line), else apply V (switch to line-wise visual mode)
+vim.keymap.set("v", "x", function()
+	if vim.fn.mode() == "V" then
+		vim.cmd("normal! j$")
+	else
+		vim.cmd("normal! V")
+	end
+end, { noremap = true })
--- .config/lvim/lua/plugins/example.lua
+++ .config/lvim/lua/plugins/example.lua
@@ -1,8 +1,54 @@
+-- Inspiration: https://github.com/benfrain/neovim/blob/main/lua/setup/lualine.lua
+
+local mode_map = {
+	["NORMAL"] = "N",
+	["O-PENDING"] = "N?",
+	["INSERT"] = "I",
+	["VISUAL"] = "V",
+	["V-BLOCK"] = "VB",
+	["V-LINE"] = "VL",
+	["V-REPLACE"] = "VR",
+	["REPLACE"] = "R",
+	["COMMAND"] = "C",
+	["SHELL"] = "SH",
+	["TERMINAL"] = "T",
+	["EX"] = "X",
+	["S-BLOCK"] = "SB",
+	["S-LINE"] = "SL",
+	["SELECT"] = "S",
+	["CONFIRM"] = "Y?",
+	["MORE"] = "M",
+}
+
+local function diff_source()
+	local gitsigns = vim.b.gitsigns_status_dict
+	if gitsigns then
+		return {
+			added = gitsigns.added,
+			modified = gitsigns.changed,
+			removed = gitsigns.removed,
+		}
+	end
+end
+
+local function place()
+	local colPre = "C:"
+	local col = "%c"
+	local linePre = " L:"
+	local line = "%l/%L"
+	return string.format("%s%s%s%s", colPre, col, linePre, line)
+end
+
+local function window()
+	return vim.api.nvim_win_get_number(0)
+end
+
 return {
 	{
 		"LazyVim/LazyVim",
 		opts = {
 			colorscheme = "catppuccin",
+			icons = {},
 		},
 	},
 	{
@@ -10,6 +56,7 @@ return {
 		lazy = true,
 		name = "catppuccin",
 		opts = {
+			flavour = "mocha",
 			integrations = {
 				aerial = true,
 				alpha = true,
@@ -48,17 +95,296 @@ return {
 				treesitter_context = true,
 				which_key = true,
 			},
-		},
-		specs = {
-			{
-				"akinsho/bufferline.nvim",
-				optional = true,
-				opts = function(_, opts)
-					if (vim.g.colors_name or ""):find("catppuccin") then
-						opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
-					end
-				end,
+			custom_highlights = {
+				CursorLine = { bg = "#2a2b3d" },
+				CursorColumn = { bg = "#2a2b3d" },
+				-- 	ColorColumn = { bg = "#313244" },
+				-- 	Whitespace = { fg = "NvimDarkGray4" },
 			},
+		},
+	},
+	{ "folke/trouble.nvim", enabled = false },
+	{ "akinsho/bufferline.nvim", enabled = false },
+	{ "folke/ts-comments.nvim", enabled = false },
+	{
+		"snacks.nvim",
+		opts = {
+			dashboard = { enabled = false },
+			-- explorer = { enabled = false },
+			indent = { enabled = false },
+		},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			-- local custom_auto = require("lualine.themes.auto")
+			-- custom_auto.normal.c.bg = "#1e1e2e" -- catppuccin color
+
+			require("lualine").setup({
+				options = {
+					icons_enabled = false,
+					theme = "auto",
+					component_separators = { " ", " " },
+					section_separators = { left = "", right = "" },
+					disabled_filetypes = {},
+				},
+				sections = {
+					lualine_a = {
+						{
+							"mode",
+							fmt = function(s)
+								return mode_map[s] or s
+							end,
+						},
+					},
+					lualine_b = {
+						{
+							"branch",
+							fmt = function(str)
+								local max_length = vim.o.columns / 3.5
+								if string.len(str) > max_length then
+									return str:sub(1, max_length) .. "..."
+								end
+								return str
+							end,
+							icon = "󰘬",
+						},
+						{
+							"diff",
+							colored = true,
+							source = diff_source,
+							diff_color = {
+								color_added = "#a7c080",
+								color_modified = "#ffdf1b",
+								color_removed = "#ff6666",
+							},
+						},
+						"diagnostics",
+					},
+					lualine_c = {
+						{
+							"filename",
+							file_status = true,
+							path = 1,
+							shorting_target = 40,
+							symbols = {
+								modified = "[+]", -- Text to show when the file is modified.
+								readonly = "[R]", -- Text to show when the file is non-modifiable or readonly.
+								unnamed = "[No Name]", -- Text to show for unnamed buffers.
+								newfile = "[New]", -- Text to show for new created file before first writting
+							},
+						},
+						{
+							"searchcount",
+						},
+						{
+							"selectioncount",
+						},
+					},
+					lualine_x = {
+						{
+							require("noice").api.status.mode.get,
+							cond = require("noice").api.status.mode.has,
+						},
+						"rest",
+						"filetype",
+						{
+							"fileformat",
+							icons_enabled = true,
+							symbols = {
+								unix = "LF",
+								dos = "CRLF",
+								mac = "CR",
+							},
+						},
+					},
+					lualine_y = { "progress" },
+					lualine_z = {
+						{ place, padding = { left = 1, right = 1 } },
+						{
+							function()
+								return os.date("%R")
+							end,
+						},
+					},
+				},
+				inactive_sections = {
+					lualine_a = { { window, color = { fg = "#26ffbb", bg = "#282828" } } },
+					lualine_b = {
+						{
+							"diff",
+							source = diff_source,
+							color_added = "#a7c080",
+							color_modified = "#ffdf1b",
+							color_removed = "#ff6666",
+						},
+					},
+					lualine_c = {
+						{
+							"filename",
+							path = 1,
+							shorting_target = 40,
+							symbols = {
+								modified = "[+]", -- Text to show when the file is modified.
+								readonly = "[R]", -- Text to show when the file is non-modifiable or read only.
+								unnamed = "[No Name]", -- Text to show for unnamed buffers.
+								newfile = "[New]", -- Text to show for new created file before first writing
+							},
+						},
+					},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = {
+						{ place, padding = { left = 1, right = 1 } },
+						{
+							function()
+								return os.date("%R")
+							end,
+						},
+					},
+				},
+				extensions = { "lazy", "fzf" },
+			})
+		end,
+	},
+	{
+		"folke/which-key.nvim",
+		icons = {
+			mappings = false,
+			keys = {},
+		},
+	},
+	{
+		"folke/noice.nvim",
+		opts = {
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
+			},
+			cmdline = {
+				enabled = true,
+				view = "cmdline",
+				format = {
+					cmdline = { icon = "❱" },
+				},
+			},
+			notify = {
+				enabled = true,
+				view = "mini",
+			},
+			messages = {
+				enabled = true,
+				view = "mini",
+				view_error = "mini", -- view for errors
+				view_warn = "mini", -- view for warnings
+			},
+		},
+	},
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("oil").setup({
+				default_file_explorer = true,
+				keymaps = {
+					["g?"] = "actions.show_help",
+					["<CR>"] = "actions.select",
+					["<C-s>"] = "actions.select_vsplit",
+					["<C-h>"] = "actions.select_split",
+					["<C-t>"] = "actions.select_tab",
+					["<C-p>"] = "actions.preview",
+					["<C-c>"] = { "actions.close", mode = "n" },
+					["<C-l>"] = "actions.refresh",
+					["-"] = "actions.parent",
+					["_"] = "actions.open_cwd",
+					["`"] = "actions.cd",
+					["~"] = "actions.tcd",
+					["gs"] = "actions.change_sort",
+					["gx"] = "actions.open_external",
+					["g."] = "actions.toggle_hidden",
+					["g\\"] = "actions.toggle_trash",
+				},
+				view_options = {
+					show_hidden = true,
+				},
+				columns = {},
+			})
+
+			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		end,
+	},
+	{
+		"echasnovski/mini.indentscope",
+		config = function()
+			require("mini.indentscope").setup({
+				draw = {
+					delay = 0,
+					animation = require("mini.indentscope").gen_animation.none(),
+				},
+				symbol = "╎",
+				mappings = {
+					-- Textobjects
+					object_scope = "ii",
+					object_scope_with_border = "ai",
+
+					-- Motions (jump to respective border line; if not present - body line)
+					goto_top = "[i",
+					goto_bottom = "]i",
+				},
+			})
+		end,
+	},
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		vscode = true,
+		---@type Flash.Config
+		opts = {},
+		keys = {
+			{
+				"gw",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "o", "x" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+			{
+				"R",
+				mode = { "o", "x" },
+				function()
+					require("flash").treesitter_search()
+				end,
+				desc = "Treesitter Search",
+			},
+		},
+	},
+	{
+		"numToStr/Comment.nvim",
+		opts = {
+			-- add any options here
 		},
 	},
 }
