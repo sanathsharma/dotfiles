49075007e339cfa36f1ed00a00a5338e35a8de0d
sanathsharma <48090208+sanathsharma@users.noreply.github.com>
2025-07-21 01:07:56 +0530

    chore: remove all old nvim configs

--- .config/nvim-vscode/lua/snippets/init.lua
+++
@@ -1,4 +0,0 @@
-require("snippets.rust")
-require("snippets.all")
-require("snippets.javascript")
-require("snippets.gitcommit")
--- .config/nvim-vscode/lua/utils/get-visual-selection.lua
+++
@@ -1,7 +0,0 @@
-local function get_visual_selection()
-	local _, ls, cs = table.unpack(vim.fn.getpos("'<"))
-	local _, le, ce = table.unpack(vim.fn.getpos("'>"))
-	return { start = { line = ls, col = cs }, ["end"] = { line = le, col = ce } }
-end
-
-return { get_visual_selection = get_visual_selection }
