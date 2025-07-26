1d96a00d1b9fca5e57a580a68a20816001c823c6
sanathsharma <48090208+sanathsharma@users.noreply.github.com>
2025-07-26 17:23:32 +0530

    feat(nvim): add mini.surround plugin with custom mappings

--- .config/nvim/lua/minimalist/lazy.lua
+++ .config/nvim/lua/minimalist/lazy.lua
@@ -353,6 +353,20 @@ require("lazy").setup({
 			})
 		end,
 	},
+	{
+		"echasnovski/mini.surround",
+		opts = {
+			mappings = {
+				add = "ms",        -- Add surrounding in Normal and Visual modes
+				delete = "md",     -- Delete surrounding
+				find = "mf",       -- Find surrounding (to the right)
+				find_left = "mF",  -- Find surrounding (to the left)
+				highlight = "mh",  -- Highlight surrounding
+				replace = "mr",    -- Replace surrounding
+				update_n_lines = "mn", -- Update `n_lines`
+			},
+		},
+	},
 	-- Themes
 	{
 		"folke/tokyonight.nvim",
