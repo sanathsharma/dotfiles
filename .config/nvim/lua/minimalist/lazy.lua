904c78834d26617c0a03ea2bdf0359a229b078e6
sanathsharma <48090208+sanathsharma@users.noreply.github.com>
2025-07-27 23:33:57 +0530

    feat(nvim): add custom keybind for blink-cmp documentation
    - Map C-k to show_documentation in blink-cmp keymap
    - Override default preset with custom documentation trigger

--- .config/nvim/lua/minimalist/lazy.lua
+++ .config/nvim/lua/minimalist/lazy.lua
@@ -201,7 +201,10 @@ require("lazy").setup({
 			-- C-k: Toggle signature help (if signature.enabled = true)
 			--
 			-- See :h blink-cmp-config-keymap for defining your own keymap
-			keymap = { preset = 'default' },
+			keymap = {
+				preset = 'default',
+				["<C-k>"] = { "show_documentation" },
+			},
 
 			appearance = {
 				-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
