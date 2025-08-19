ff0c0362e2014893c5652301fd2c1f23a54f3d8e
sanathsharma <48090208+sanathsharma@users.noreply.github.com>
2025-08-19 23:50:57 +0530

    feat(fish): switch default editor from helix to nvim
    - Update EDITOR environment variable to nvim
    - Update VISUAL environment variable to nvim
    - Update GIT_EDITOR environment variable to nvim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -46,9 +46,9 @@ alias switch-branch='sh ~/scripts/checkout-branch.sh'
 alias dr="sh ~/scripts/debug_rust.sh"
 
 # Set variables
-set -gx EDITOR hx
-set -gx VISUAL hx
-set -gx GIT_EDITOR hx
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+set -gx GIT_EDITOR nvim
 set -gx GOPATH $HOME/go
 set -gx STARSHIP_CONFIG $HOME/.config/starship/starship.toml
 set -gx GPG_TTY "$(tty)"
