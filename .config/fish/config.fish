aacb33e150dd5f123f414e661e9fe99ed49eedc0
sanathsharma <48090208+sanathsharma@users.noreply.github.com>
2025-08-22 21:00:33 +0530

    feat(fish): add OrbStack shell integration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -150,3 +150,7 @@ function w
 		cd $result
 	end
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
