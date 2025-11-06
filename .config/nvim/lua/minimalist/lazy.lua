ade3531ea079abed07b8fe2905466cd9960fbeed
sanathsharma <48090208+sanathsharma@users.noreply.github.com>
2025-11-07 00:28:01 +0530

    feat(nvim): enhance blink-cmp keymap configuration and disable preselection
    - Switch from default preset to custom keymap bindings
    - Add comprehensive navigation keys for completion menu
    - Configure documentation scrolling and snippet navigation
    - Update signature help trigger from C-k documentation to show/hide signature
    - Disable preselection in completion and cmdline modes

--- .config/nvim/lua/minimalist/lazy.lua
+++ .config/nvim/lua/minimalist/lazy.lua
@@ -219,8 +219,23 @@ require("lazy").setup({
 			--
 			-- See :h blink-cmp-config-keymap for defining your own keymap
 			keymap = {
-				preset = "default",
-				["<C-k>"] = { "show_documentation" },
+				preset = "none",
+				["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+				["<C-e>"] = { "hide", "fallback" },
+				["<C-y>"] = { "select_and_accept", "fallback" },
+
+				["<Up>"] = { "select_prev", "fallback" },
+				["<Down>"] = { "select_next", "fallback" },
+				["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+				["<C-n>"] = { "select_next", "show", "fallback" },
+
+				["<C-b>"] = { "scroll_documentation_up", "fallback" },
+				["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+				["<Tab>"] = { "snippet_forward", "fallback" },
+				["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+				["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
 			},
 
 			appearance = {
@@ -232,14 +247,14 @@ require("lazy").setup({
 			completion = {
 				-- (Default) Only show the documentation popup when manually triggered
 				documentation = { auto_show = true },
-				list = { selection = { auto_insert = true, preselect = true } },
+				list = { selection = { auto_insert = true, preselect = false } },
 				accept = { auto_brackets = { enabled = false } },
 				menu = { auto_show = true },
 			},
 
 			cmdline = {
 				completion = {
-					list = { selection = { auto_insert = true, preselect = true } },
+					list = { selection = { auto_insert = true, preselect = false } },
 					menu = { auto_show = true },
 				},
 				keymap = {
