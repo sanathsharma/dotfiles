e858accf268e62197313b5b1336a0ca62dbe0736
sanathsharma <48090208+sanathsharma@users.noreply.github.com>
2026-01-17 15:16:02 +0530

    feat: update kanata home row mods to use left alt and add fzf-lua ui select registration in nvim

--- .config/nvim/lua/minimalist/lazy.lua
+++ .config/nvim/lua/minimalist/lazy.lua
@@ -36,6 +36,25 @@ require("lazy").setup({
 	{
 		"ibhagwan/fzf-lua",
 		config = function()
+			-- local actions = require("fzf-lua.actions")
+			-- local actions_map = {
+			-- -- Pickers inheriting these actions:
+			-- --      files, git_files, git_status, grep, lsp, oldfiles, quickfix, loclist,
+			-- --      tags, btags, args, buffers, tabs, lines, blines
+			-- --    `file_edit_or_qf` opens a single selection or sends multiple selection to quickfix
+			-- --    replace `enter` with `file_edit` to open all files/bufs whether single or multiple
+			-- --    replace `enter` with `file_switch_or_edit` to attempt a switch in current tab first
+			-- 	["enter"] = actions.file_edit_or_qf,
+			-- 	["ctrl-s"] = actions.file_split,
+			-- 	["ctrl-v"] = actions.file_vsplit,
+			-- 	["ctrl-t"] = actions.file_tabedit,
+			-- 	["alt-q"] = actions.file_sel_to_qf,
+			-- 	["alt-Q"] = actions.file_sel_to_ll,
+			-- 	["alt-i"] = actions.toggle_ignore,
+			-- 	["alt-h"] = actions.toggle_hidden,
+			-- 	["alt-f"] = actions.toggle_follow,
+			-- }
+
 			require("fzf-lua").register_ui_select()
 			require("fzf-lua").setup({
 				winopts = {
