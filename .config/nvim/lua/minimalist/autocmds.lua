5f6ea6931bb50824ae1599bf1b163c13d05a49f6
sanathsharma <48090208+sanathsharma@users.noreply.github.com>
2026-07-23 11:11:48 +0530

    fix(nvim): add autocmd to disable inlay_hints in insert mode for the current buffer when its enabled

--- .config/nvim/lua/minimalist/autocmds.lua
+++ .config/nvim/lua/minimalist/autocmds.lua
@@ -221,3 +221,23 @@ vim.api.nvim_create_autocmd("Filetype", {
 		vim.opt.spell = true
 	end,
 })
+
+-- Mangage Inlay hints in insert mode
+-- See https://github.com/neovim/neovim/discussions/29078
+vim.api.nvim_create_autocmd("InsertEnter", {
+	desc = "Disable lsp.inlay_hint when in insert mode",
+	callback = function(args)
+		local filter = { bufnr = args.buf }
+		local inlay_hint = vim.lsp.inlay_hint
+		if inlay_hint.is_enabled(filter) then
+			inlay_hint.enable(false, filter)
+			vim.api.nvim_create_autocmd("InsertLeave", {
+				once = true,
+				desc = "Re-enable lsp.inlay_hint when leaving insert mode",
+				callback = function()
+					inlay_hint.enable(true, filter)
+				end,
+			})
+		end
+	end,
+})
